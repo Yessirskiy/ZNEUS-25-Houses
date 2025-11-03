67d476c5206d6b1f630f7beed8eb33230f0d99bd
filtom2 <filtom@proton.me>
2025-11-03 21:22:06 +0100

    added model, training loop, graphing and dataset /w pre-processing applied

--- project/dataset.py
+++ project/dataset.py
@@ -2,34 +2,31 @@ import torch
 import numpy as np
 from torch.utils.data import Dataset
 
+#TODO instead of applying transfomations in ipynb file pickle them and reuse them instead
 
 class HousingDataset(Dataset):
-    def __init__(self, dataframe, stats, target_col='median_house_value'):
+    def __init__(self, dataframe, stats, target_col, log_cols_exclude, min_max_cols):
         self.stats = stats
         self.target_col = target_col
         
-        # Deep copy to avoid modifying the original dataframe in place
         df = dataframe.copy()
         
-        # 1. Apply Log Transform - All except lat/lon
-        log_cols = [col for col in df.columns if col not in ['latitude', 'longitude']]
+        # 1. Apply Log Transform 
+        log_cols = [col for col in df.columns if col not in log_cols_exclude]
         
         for col in log_cols:
             df[col] = np.log1p(df[col])
             
-        # 2. Apply Normalization - Min-Max cols
-        min_max_cols = ['median_house_value', 'housing_median_age']
+        # 2. Apply Normalization - Min-Max cols 
         for col in min_max_cols:
+            if col not in self.stats: continue # Skip if stats not present (e.g. target in test set)
             min_val = self.stats[col]['min']
             max_val = self.stats[col]['max']
-            # Epsilon for math. stability  (division by zero if min == max)
-            df[col] = (df[col] - min_val) / (max_val - min_val + 1e-7)
+            df[col] = (df[col] - min_val) / (max_val - min_val)
             
         # Z-Score cols
         feature_cols = [col for col in df.columns if col != self.target_col]
-        z_score_cols = [
-            col for col in feature_cols if col not in ['housing_median_age']
-        ]
+        z_score_cols = [col for col in feature_cols if col not in min_max_cols]
 
         for col in z_score_cols:
             mean_val = self.stats[col]['mean']
@@ -37,7 +34,7 @@ class HousingDataset(Dataset):
             # Add epsilon to prevent division by zero
             df[col] = (df[col] - mean_val) / (std_val + 1e-7)
         
-        # 3. Store as Tensors 
+        # --- 3. Store as Tensors ---
         self.y = torch.tensor(df[self.target_col].values, dtype=torch.float32)
         
         # Get all feature columns after processing
@@ -48,8 +45,8 @@ class HousingDataset(Dataset):
         return len(self.y)
 
     def __getitem__(self, idx):
-        """
-        Generates one sample of data.
-        Returns features and target as a tuple.
-        """
-        return self.X[idx], self.y[idx].unsqueeze(0) # Unsqueeze to make target shape (1,)
+        # Unsqueeze the target to make its shape [1] instead of scalar
+        return self.X[idx], self.y[idx].unsqueeze(0)
+
+    def get_feature_count(self):
+        return self.X.shape[1]
